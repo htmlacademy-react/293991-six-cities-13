d6875886b16240ebb552bb44454b3c13b354e884
fikfok <sadovenko.dmitry@gmail.com>
2023-07-14 02:06:00 +0300

    Добавил стили для Not found page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,15 +1,20 @@
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import styles from './not-found-page.module.css';
 
 function NotFoundPage(): JSX.Element {
   return (
-    <>
+    <div className={`page ${styles.notFoundPage}`}>
       <Helmet>
         <title>6 cities. Page not found</title>
       </Helmet>
-      <h1>404 Not Found</h1>
-      <Link to="/">Go to main page</Link>
-    </>
+      <h1 className={styles.title}>404 Not Found</h1>
+      <p className={styles.text}>
+        Return to the {' '}
+        <Link to={AppRoute.Root} className={styles.link}>main page</Link>
+      </p>
+    </div>
   );
 }
 
