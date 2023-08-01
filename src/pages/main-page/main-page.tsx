e1e8004aed31eadef6c356e04af5ce11eb4d27ba
fikfok <sadovenko.dmitry@gmail.com>
2023-08-01 23:38:30 +0300

    Добавление компонента сортировки

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -8,6 +8,7 @@ import { CITIES } from '../../const';
 import CitiesTabList from '../../components/cities-tab-list/cities-tab-list';
 import { useAppSelector } from '../../hooks';
 import { getOffersByCity } from '../../utils/utils';
+import OffersSorting from '../../components/offers-sorting/offers-sorting';
 
 function MainPage (): JSX.Element {
   const allOffersShort = useAppSelector((state) => state.offers);
@@ -68,32 +69,7 @@ function MainPage (): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{offersShort.length} places to stay in {activeCity.name}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <OffersSorting/>
               <div className="cities__places-list places__list tabs__content">
                 {offersShort.map((offerShort: OfferShort) => (<OfferCard key={offerShort.id} offerShort={offerShort} mode={OfferCardMode.MainPage} onMouseEnterHandler={onMouseEnterHandler(offerShort.id)}/>))}
               </div>
